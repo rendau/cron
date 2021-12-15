fa03c6adacd9093cd78c90c962f1b763b7e861d9
mdb <dauren.asir@gmail.com>
2021-12-15 10:49:26 +0600

    init

--- cmd/root.go
+++ cmd/root.go
@@ -1,7 +1,6 @@
 package cmd
 
 import (
-	"fmt"
 	"log"
 	"os"
 	"os/signal"
@@ -10,66 +9,60 @@ import (
 	"github.com/rendau/cron/internals/adapters/logger/zap"
 	"github.com/rendau/cron/internals/domain/core"
 	"github.com/rendau/cron/internals/domain/entities"
-	"github.com/spf13/cobra"
+	"github.com/rendau/cron/internals/interfaces"
 	"github.com/spf13/viper"
 )
 
-// rootCmd represents the base command when called without any subcommands
-var rootCmd = &cobra.Command{
-	Use: "cron",
-	Run: func(cmd *cobra.Command, args []string) {
-		loadConfig()
-
-		lg, err := zap.New(viper.GetString("log_level"), viper.GetBool("debug"), false)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		jobs := viper.Get("parsed_jobs").([]*entities.JobSt)
-
-		core := core.New(lg, jobs)
-
-		lg.Infow("Starting", "http_listen", viper.GetString("http_listen"))
-
-		for _, job := range jobs {
-			lg.Infow(
-				"Cron job",
-				"time", job.Time,
-				"url", job.Url,
-			)
-		}
-
-		err = core.StartCron()
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		stop := make(chan os.Signal, 1)
-		signal.Notify(stop, os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
-
-		<-stop
-
-		lg.Infow("Shutting down...")
-
-		core.StopCron()
-
-		os.Exit(0)
-	},
-}
-
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-	if err := rootCmd.Execute(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+	var err error
+
+	app := struct {
+		lg   interfaces.Logger
+		core *core.St
+	}{}
+
+	loadConfig()
+
+	app.lg, err = zap.New(viper.GetString("LOG_LEVEL"), viper.GetBool("DEBUG"), false)
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	jobs := viper.Get("PARSED_JOBS").([]*entities.JobSt)
+
+	app.core = core.New(app.lg, jobs)
+
+	app.lg.Infow("Starting", "http_listen", viper.GetString("HTTP_LISTEN"))
+
+	for _, job := range jobs {
+		app.lg.Infow(
+			"Cron job",
+			"time", job.Time,
+			"url", job.Url,
+		)
+	}
+
+	err = app.core.StartCron()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
+
+	<-stop
+
+	app.lg.Infow("Shutting down...")
+
+	app.core.StopCron()
+
+	os.Exit(0)
 }
 
 func loadConfig() {
-	viper.SetDefault("debug", "false")
-	viper.SetDefault("http_listen", ":9090")
-	viper.SetDefault("log_level", "debug")
+	viper.SetDefault("DEBUG", "false")
+	viper.SetDefault("HTTP_LISTEN", ":9090")
+	viper.SetDefault("LOG_LEVEL", "debug")
 
 	confFilePath := os.Getenv("CONF_PATH")
 	if confFilePath == "" {
@@ -81,10 +74,10 @@ func loadConfig() {
 	viper.AutomaticEnv()
 
 	jobs := make([]*entities.JobSt, 0)
-	err := viper.UnmarshalKey("jobs", &jobs)
+	err := viper.UnmarshalKey("JOBS", &jobs)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	viper.Set("parsed_jobs", jobs)
+	viper.Set("PARSED_JOBS", jobs)
 }
--- go.mod
+++ go.mod
@@ -1,10 +1,26 @@
 module github.com/rendau/cron
 
-go 1.15
+go 1.17
 
 require (
 	github.com/robfig/cron v1.2.0
-	github.com/spf13/cobra v1.0.0
 	github.com/spf13/viper v1.4.0
 	go.uber.org/zap v1.10.0
+)
+
+require (
+	github.com/fsnotify/fsnotify v1.4.7 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/magiconair/properties v1.8.0 // indirect
+	github.com/mitchellh/mapstructure v1.1.2 // indirect
+	github.com/pelletier/go-toml v1.2.0 // indirect
+	github.com/spf13/afero v1.1.2 // indirect
+	github.com/spf13/cast v1.3.0 // indirect
+	github.com/spf13/jwalterweatherman v1.0.0 // indirect
+	github.com/spf13/pflag v1.0.3 // indirect
+	go.uber.org/atomic v1.4.0 // indirect
+	go.uber.org/multierr v1.1.0 // indirect
+	golang.org/x/sys v0.0.0-20190215142949-d0b11bdaac8a // indirect
+	golang.org/x/text v0.3.0 // indirect
+	gopkg.in/yaml.v2 v2.2.2 // indirect
 )
